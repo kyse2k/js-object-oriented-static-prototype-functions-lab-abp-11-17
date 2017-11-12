cf049884ef0b607621e234e46913a3373f4a455d
Sérgio Pereira <sergiodanielpereira@gmail.com>
2017-11-12 22:13:04 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,4 +1,8 @@
 function IcebreakerResponse(email){
   this.email = email
 }
-
+Icebreaker.BatchCreate = function (emails){
+  return emails.map(function(email){
+    new IcebreakerResponse(email)
+  })
+}
